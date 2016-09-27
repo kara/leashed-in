d15623571c285c1163e3e70f01bc50c8a566a3b1
Kara Erickson <karakara@google.com>
2016-09-27 13:05:58 +0100

    code from live demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import {MdSidenav, MdDialog, MdDialogConfig} from "@angular/material";
+
+@Component({
+  selector: 'settings-dialog',
+  template: `
+    <label>Would you like dog pics every min???</label>
+    <md-slide-toggle></md-slide-toggle>
+  `
+})
+export class SettingsDialog {
+
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  dogs = [
+    {rows: 2, name: "Mal", human: "Jeremy", age: 5},
+    {rows: 1, name: "Molly", human: "David", age: 5},
+    { rows: 1, name: "Sophie", human: "Alex", age: 8},
+    {rows: 2, name: "Taz", human: "Joey", age: '11 weeks'},
+    { rows: 1, name: "Kobe", human: "Igor", age: 5},
+    {rows: 2, name: "Porter", human: "Kara", age: 3},
+    { rows: 1, name: "Stephen", human: "Stephen", age: 8},
+    {rows: 1, name: "Cinny", human: "Jules", age: 3},
+    { rows: 1, name: "Hermes", human: "Kara", age: 3},
+  ];
+  @ViewChild('sidenav') sidenav: MdSidenav;
+  currentDog = {};
+  isDarkTheme = false;
+
+  constructor(public dialog: MdDialog, public vcr: ViewContainerRef) {}
+
+  openDialog() {
+    const config = new MdDialogConfig();
+    config.viewContainerRef = this.vcr;
+    this.dialog.open(SettingsDialog, config);
+  }
+
+  showDog(dog) {
+    this.currentDog = dog;
+    this.sidenav.open();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
+import { AppComponent, SettingsDialog } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsDialog
+  ],
+  entryComponents: [
+    AppComponent,
+    SettingsDialog
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
