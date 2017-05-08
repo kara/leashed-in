827ce15a7213cfc645c76437412284e9890bc7d5
Dmitry Teplov <teplov.work@gmail.com>
2017-05-08 04:40:18 +0300

    Updated packages and project files.
    Updated packages to latest available versions.
    Updated angular CLI config to work with new version.
    Fixed karma config due to CLI update.
    Fixed source files so they compile correctly.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -16,6 +17,7 @@ import { AppComponent, SettingsDialog } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     MaterialModule.forRoot()
